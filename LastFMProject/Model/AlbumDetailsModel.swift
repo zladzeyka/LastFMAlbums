41173f7a1a7cb83a77c02d9a5c7806b0f322dba0
zladzeyka <nadyakaraban@gmail.com>
2020-08-18 19:45:34 +0200

    added model for Album details page
    added model for Album details page

---
+++ LastFMProject/Model/AlbumDetailsModel.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumDetailsModel.swift
+//  LastFMProject
+//
+//  Created by Nadzeya Karaban on 14.07.20.
+//  Copyright © 2020 Nadzeya Karaban. All rights reserved.
+//
+
+import Foundation
+struct AlbumDetailsModel: Codable {
+    var name: String
+    var artist: String
+    var image: String
+}
